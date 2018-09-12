f87fde9f708af94b9eea0866fd70506bd1a93f82
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-12 11:31:13 +0000

    Lesson 9 beginning

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-var people = [james, mary, ryu];
-
-people.forEach(function(person){
-    person.on('speak', function(mssg){
-        console.log(person.name + ' said: ' + mssg);
-    });
-});
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', 'I want a curry');
+var readMe = fs.readFileSync('readMe.txt', 'utf8');
+fs.writeFileSync('writeMe.txt', readMe);
