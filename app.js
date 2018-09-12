b15c8995afcdbe59054d6b417c13a6de83baba00
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-12 11:34:25 +0000

    Lesson 9 complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
 
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
-fs.writeFileSync('writeMe.txt', readMe);
+fs.readFile('readMe.txt', 'utf8', function(err, data){
+fs.writeFile('writeMe.txt', data);
+});
