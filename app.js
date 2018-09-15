e0108c90ec497bb0d3a239514b0656be9e91daa3
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-15 17:25:31 +0000

    lesson 19 basic routing

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 console.log('request was made: ' + req.url);
-res.writeHead(200, {'Content-Type': 'application/json'});
-var myObj = {
-    name: 'Ryu',
-    job: 'Ninja',
-    age: 29
-};
-res.end(JSON.stringify(myObj));
-
+if(req.url === '/home' || req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(_dirname + '/index.html').pipe(res);
+} else if(req.url === '/contact'){
+fs.createReadStream(_dirname + '/contact.html').pipe(res);
+} else if(req.url === '/api/ninjas'){
+    var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   res.end(JSON.stringify(ninjas));
+} else {
+     res.writeHead(404, {'Content-Type': 'text/html'});
+     fs.createReadStream(_dirname + '/contact.html').pipe(res);
+}
 });
 
 server.listen(3000, '127.0.0.1');
 //his local IP address 
-console.log('yo dawgs, now listening to port 3000');
+
