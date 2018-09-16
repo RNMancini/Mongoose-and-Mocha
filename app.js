db345014d6437d1d422f16c75daa8e1f25242594
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-16 03:11:29 +0000

    lesson 20-23

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-var server = http.createServer(function(req, res){
-console.log('request was made: ' + req.url);
-if(req.url === '/home' || req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(_dirname + '/index.html').pipe(res);
-} else if(req.url === '/contact'){
-fs.createReadStream(_dirname + '/contact.html').pipe(res);
-} else if(req.url === '/api/ninjas'){
-    var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}];
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   res.end(JSON.stringify(ninjas));
-} else {
-     res.writeHead(404, {'Content-Type': 'text/html'});
-     fs.createReadStream(_dirname + '/contact.html').pipe(res);
-}
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('this is the homepage');
 });
 
-server.listen(3000, '127.0.0.1');
-//his local IP address 
+app.get('/contact', function(req, res){
+    res.send('this is the contact page');
+});
 
+app.listen(3000);
