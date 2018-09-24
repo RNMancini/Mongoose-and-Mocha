d5ba644cf0e306a81a0464a680032ba8225a886a
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-24 03:10:26 +0000

    lesson 30

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var app = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencoderParses = bodyParser.urlencoded({ extended:})
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('stuff'));
@@ -9,7 +12,12 @@ app.get('/', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-    res.render('contact');
+    res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencoderParser, function(req, res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res){
