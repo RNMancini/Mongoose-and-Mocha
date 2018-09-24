0e09afcc2b7618d353937cf55601ec3648ca3e69
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-24 21:57:12 +0000

    making todo app part two

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-var app = require('express');
-var bodyParser = require('body-parser');
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
 var app = express();
 
-var urlencoderParses = bodyParser.urlencoded({ extended:})
-
+//set up template engine
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('stuff'));
 
-app.get('/', function(req, res){
-  res.render('index');
+//static files
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+ todoController(?)
+
 });
-
-app.get('/contact', function(req, res){
-    res.render('contact', {qs: req.query});
-});
-
-app.post('/contact', urlencoderParser, function(req, res){
-    console.log(req.body);
-    res.render('contact-success', {data: req.body});
-});
-
-app.get('/profile/:name', function(req, res){
-   var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-    res.render('profile' + {person: req.params.name});
-});
-
+//listen to port
 app.listen(process.env.PORT, process.env.IP);
