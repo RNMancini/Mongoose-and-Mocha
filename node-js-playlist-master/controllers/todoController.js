4700482a09ab0b385bfd55772247c4a76c80416a
RNMancini <rebeccamancini.student@careerdevs.com>
2018-09-25 19:02:30 +0000

    Database created, Mongoose installed

--- node-js-playlist-master/controllers/todoController.js
+++ node-js-playlist-master/controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect('mongodb://<test>:test12@ds113703.mlab.com:13703/todo-example');
+
+//Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
